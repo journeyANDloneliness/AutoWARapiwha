a2debc3309cd6c181de7ba154264bee75e524811
journeyANDloneliness <rumetta.may@gmail.com>
2023-06-23 06:24:39 +0000

    no websocket

--- src/index (copy).ts
+++ src/index (copy).ts
@@ -1,11 +1,24 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-
+import WebSocket from 'ws';
 import  http from 'http';
 
 const app = express()
+let ws=null
 
+let socketBerjalan=false
+export const runSocket =async ()=>{
+  const server = http.createServer(app);
+  const wss = new WebSocket.Server({ server });
+  socketBerjalan = true
+  await wss.on('connection', (w) => {
+    
+    console.log('Client connected');
+    ws=w
+    w.send('anda terhubung dengan server!'); // send a message to the client
+  });
+}
 
 app.use(cors())
 const port = 3000
@@ -13,7 +26,61 @@ const port = 3000
 import Database from "@replit/database"
 const db = new Database()
 
+//app.use(bodyParser.text());
 
+export default (toDo)=>{
+app.post('/', function(req, res) {
+  let data = '';
+  req.on('data', (chunk) => {
+    data += chunk;
+  });
+  req.on('end', async () => {
+    
+      var decodedData =
+        decodeURIComponent(data.replaceAll("+"," "));
+ 
+      let dataJson = JSON.parse(decodedData.slice(5));
+      console.log(dataJson)
+      if (dataJson.event==="INBOX"){
+        let response = {autoreply:""}
+        response.autoreply = toDo({pesan:dataJson.text,dari:dataJson.from, ke:dataJson.to,
+                                  database:db})
+        res.json(response)
+      }
+  
+  })
+
+  
+});
+      
+
+
+app.listen(port, () => {
+  // Code.....
+})
+}
+
+export const AutoWA2 = (toDo)=>{
+app.use(bodyParser.json());
+app.post('/', function(req, res) {
+    console.log(req.body)
+    let dataJson =  req.body
+      if (dataJson.event==="INBOX"){
+        let response = {autoreply:{pesan:""}}
+        response.autoreply.pesan = toDo({pesan:dataJson.text,dari:dataJson.from, ke:dataJson.to,
+                                  database:db})
+        res.json(response)
+      }
+  
+
+
+  
+});
+  app.listen(port, () => {
+  // Code.....
+})
+
+}
 let serverBerjalan=false
 
 let toResolve=[]
